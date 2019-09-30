9b9e41d254a801909c0dbd8d273c3acfeb3de11a
Kiril0l <Kirill_1998_12@mail.ru>
2019-09-30 21:17:28 +0300

    fg

--- dem.py
+++ dem.py
@@ -9,45 +9,32 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 5000
-PID_FILE = "/var/run/step/demon/demon.pid"
+PID_FILE = "/var/run/demon.pid"
 WORK = True
 
 
-def demon():
-    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    srv.bind((HOST, PORT))
-    srv.listen(20)   #очередь обращений
-    storage = {}
-    try:
-        sock = storage[key]
-    except KeyError:
-        print(f"Key {key} not found.")
-        return
-    try:
-        while True:
-            data = sock.recv(2048)
-            response = data.decode("utf-8")
-            print(client, response)
-            if "close" in response:
-                break
-            if "demon_close" in response:
-                lambda: os.kill(get_pid(), signal.SIGKILL)
-            for client, client_sock in storage.items():
-                if client != key:
-                    client_sock.send(f"{client[0]}\n".encode("utf-8"))
-                    client_sock.send(response.encode("utf-8"))
-    except Exception as e:
-        print(e)
-    finally:
-        sock.close()
-        del storage[key]
-
-def get_pid():
-    pid = 0
-    with open(PID_FILE, "r") as pid_file:
-        pid = int(pid_file.readline())
-    return pid
+srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+srv.bind((HOST, PORT))
+srv.listen(20)   #очередь обращений
+try:
+    client, addr = srv.accept()  #установка соединения(данные о соединении)
+    for index in range(1, 10):
+        data = client.recv(2048)   #принимаем данные(объем в байтах)
+        data_str = data.decode("utf-8")
+        print(data)
+        if "close" in data_str:
+            break
+        if "demon_close" in data_str:
+            lambda: os.kill(get_pid(), signal.SIGKILL)
+        if not data:
+            break
+        result = f"{data}\n"
+        client.send(data)    #возвращает нащи данные
+except Exception as e:
+    print(e)
+finally:
+    srv.close()
 
 
 def start_demon():
@@ -67,8 +54,51 @@ def start_demon():
     else:
         demon()
 
+def demon():
+    def handler(signum, frame):
+        with open("signal.log", "a") as log_file:
+            log_file.write(f"Signal: {signum}\n{frame}\n\n")
+
+    signal.signal(signal.SIGUSR1, handler)
+    signal.signal(signal.SIGUSR2, handler)
+
+
+    while True:
+        try:
+            with open("demon.log", "a") as log_file:
+                log_file.write(f"{datetime.now()}\n\n")
+        except KeyboardInterrupt:
+            with open("signal.log", "a") as log_file:
+                log_file.write("Ctrl + C")
+        except Exception as e:
+            pass
+        finally:
+            time.sleep(randint(5, 15))
+
+
+def get_pid():
+    pid = 0
+    with open(PID_FILE, "r") as pid_file:
+        pid = int(pid_file.readline())
+    return pid
+
+
 if __name__ == '__main__':
-    start_demon()
+    try:
+        os.mkdir(os.path.join(*os.path.split(PID_FILE)[:-1]))
+    except FileNotFoundError:
+        try:
+            os.makedirs(os.path.join(*os.path.split(PID_FILE)[:-1]))
+        except PermissionError:
+            print("WTF !!!")
+            sys.exit(1)
+    except FileExistsError:
+        pass
+    args = sys.argv[1:]
+    if len(args):
+        send_signal(args[0])
+    else:
+        start_demon()
 
 
 # демон
